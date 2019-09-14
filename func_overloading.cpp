48c8eb9bb8edf34d87757d4e707ba2c9535ca7a7
Nitin Kumar Yadav <31594344+im-Nky@users.noreply.github.com>
2019-09-15 00:13:38 +0530

    function overloading

---
+++ func_overloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class box {
+        public:
+                int area (int l,int b)
+                {
+                        return (l*b);
+                }
+                float area (int i)
+                {
+                        return (3.14*i*i);
+                }
+};
+
+class rectangle: public shape {
+        public:
+                int volume();           
+};
+
+int main()
+{
+        box b1;
+        cout<<"The area of circle "<<b1.area(4)<<endl;
+        cout<<"The area of rectangle  "<<b1.area(3,4)<<endl;
+}
+
