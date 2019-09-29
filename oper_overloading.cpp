241f3d74681d4e3ba9eea99c9055320dedbd2d25
Nitin Kumar Yadav <31594344+im-Nky@users.noreply.github.com>
2019-09-29 11:19:08 +0530

    Operator Overloading
    Sum of Complex number: Changing behaviour of + operator

---
+++ oper_overloading.cpp
@@ -0,0 +1,26 @@
+// Operator Overlaoding: sum of complex numbers
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Complex {
+public:
+	int real,img;
+	Complex(int a, int b):real{a},img{b}{ }
+	void print(){cout<<real<<"+i"<<img<<endl;}
+	Complex operator + (Complex const &obj) {
+		//Complex res;
+		//res.real = real+obj.real;
+		//res.img = img+obj.img;
+		return Complex((real+obj.real),(img+obj.img));
+		//return res;
+	}
+};
+
+int main()
+{
+	Complex c1(10,5),c2(2,4);
+	Complex c3=c1+c2;
+	c3.print();
+	return 0;
+}
