f70b70c6465c3ced3d9ab8ef94ddb9bb57510ffe
Nitin Kumar Yadav <31594344+im-Nky@users.noreply.github.com>
2019-11-28 11:26:34 +0530

    Data Hiding in C++

---
+++ data_hiding.cpp
@@ -0,0 +1,27 @@
+/*
+	Demonstration of Data Hiding
+*/
+
+#include <iostream>
+using namespace std;
+
+class A{
+	int a,b;
+	public:
+	void set(int _a, int _b){		/*Data Hiding*/
+		a = _a;
+		b = _b;
+	}
+	void print()
+	{
+		cout << a <<" "<< b <<endl;
+	} 
+};
+
+int main()
+{
+	A a1;
+	a1.set(2,3);
+	a1.print();
+	return 0;
+}
