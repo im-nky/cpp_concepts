b7c38c320c89b0a7dd544822c55ecaa561eaffbc
Nitin Kumar Yadav <im_nky@yahoo.com>
2020-01-10 11:18:20 +0530

    Virtual Function

--- 6.vir_func.cpp
+++ 6.vir_func.cpp
@@ -1,5 +1,11 @@
 /*
-	Virtual Function: 
+	Virtual Function: Giving new implementation to base class method into derived class and 
+	accessing it through base class pointer is known as function overriding.
+	
+	Base class pointer: A pointer which can hold address of it's child or derived class objects.
+	
+	Virtual keyword indicates compiler late binding has to be performed i.e. memory allocation 
+	will take place at runtime.
 */
 
 #include<bits/stdc++.h>
