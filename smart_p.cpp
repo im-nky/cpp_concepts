4e32f9c20c724a7233e742069ee04dd1f44c80b4
Nitin Kumar Yadav <im_nky@yahoo.com>
2020-07-30 16:35:23 +0530

    Smart_Pointer_Concept

---
+++ smart_p.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class myint {
+	private:
+		int * data;
+	public:
+		myint(int * p = NULL) { data = p;}
+		~ myint() {delete(data);}
+		int operator * () {return * data;}
+};
+
+
+
+int main() {
+	int * p = new int (5);
+	myint m = myint(p);
+	cout << *m << endl;
+	return 0;
+}
